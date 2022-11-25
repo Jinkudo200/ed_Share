50220d7b88d7338eea4ef05a20c9ec30f8bcb260
Jinkudo200 <aymanelkihel1@gmail.com>
2022-11-25 22:08:50 +0100

    imports

---
+++ src/Components.js/Input.js
@@ -0,0 +1,45 @@
+import React , {useState} from 'react'
+import {ref , uploadBytes} from 'firebase/storage'
+import { db, storage } from '../config/firebase'
+
+
+export const MyInput=({formProps,field,header})=>(
+    <div className='input'>
+      <p >{header}</p>
+      <input 
+      // style={{borderBottomColor:formProps.isSubmitting&&formProps.errors[field]?"red":"black"}}
+      onChange={formProps.handleChange(field)}
+      value={formProps.values[field]}
+      onBlur={formProps.handleBlur(field)}
+      // {...props}
+      />
+      <p style={{color:"red"}}>{formProps.touched[field]&&formProps.errors[field]}</p>
+    </div>
+  )
+  
+  
+  
+  
+  export const FileInput=(props)=>{
+    const [data, setData] = useState(null)
+    const handlechange=(e)=>{
+      const defaultChange = props.formProps.handleChange(props.field);
+      setData(e.target.files[0])
+    //   if(data===null) return;
+    //   console.log("uploading image")
+    //   const d = e.target.files[0];
+    //   console.log(d)
+    //   uploadBytes(ref(storage,`/images/${d.name}`),d)
+    //   .then(e=>{console.log(e);
+        // })
+        defaultChange(e)
+    }
+    return(
+      (
+        <div>
+          <p>{props.header}</p>
+          <input type='file'onChange={e=>handlechange(e)}/>
+        </div>
+      )
+    )
+  }
--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import { MyInput , FileInput } from '../Components.js/Input';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
 
-export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
-}
+
+export default function Contact (){
+
+
+  const handleSubmit=(values)=>{
+    // addDoc(collection(db,'users'),{
+    //   email:values.email,
+    //   name:values.firstName+' '+values.lastName,
+    //   imgUrl:values.imgUrl,
+    //   quote:"hello there",
+    //   nbrprojects:0
+    // })
+    // .then(docRef => console.log("Document written with ID: ", docRef.id))
+    // .catch(e=>console.log(e))
+    console.log(values)
+  }
+  // const handle
+
+    return (
+      <Formik
+        initialValues={{ firstName: '', lastName: '', email: '',imgUrl:''}}
+        validationSchema={Yup.object({
+          firstName: Yup.string().required('Required'),
+          lastName: Yup.string().required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {handleSubmit(values)
+        }}
+      >
+        {(formProps)=>(
+          <Form>
+        <div className='form-container'>
+          <MyInput formProps={formProps} field="firstName" header='First Name'/>
+          <MyInput formProps={formProps} field="lastName" header='Last Name'/>
+          <MyInput formProps={formProps} field="email" header='Email'/>
+          <FileInput formProps={formProps} field="imgUrl" header="Image"/>
+
+          <button type="submit">Submit</button>
+          </div>
+          </Form>
+        )}
+      </Formik>
+    );
+  };
