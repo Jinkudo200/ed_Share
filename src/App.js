fe3893909ae594d77ba7e8cc2b1121f4a215d7ca
Abderafie <chairi.abderafie@student.emi.ac.ma>
2022-11-27 17:10:47 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import MainRouter from './routers/MainRouter';
 function App() {
   return (
     <div className="App">
-      <MainRouter/>
+      <p>test githu pages</p>
     </div>
   ); 
 }
+      {/* <MainRouter/> */}
 
 export default App;
